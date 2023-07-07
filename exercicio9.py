f14ccf416cef8a4b0f53ca23627f8efab0cc3100
Markus Vinicius <85716855+mviniortiz@users.noreply.github.com>
2023-07-06 22:51:23 -0300

    Create exercicio9.py

---
+++ exercicio9.py
@@ -0,0 +1,8 @@
+num_funcionario = int(input())
+num_horas_trabalhadas = int(input())
+valor_hora_trabalhada = float(input())
+
+salario = num_horas_trabalhadas * valor_hora_trabalhada
+
+print ('NUMBER = {}'.format(num_funcionario))
+print('SALARY = U$ {:.2f}'.format(salario))
